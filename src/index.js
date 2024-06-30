e769b7086dac52fc85078b2d8b0d5e5b3fc4b55a
cordelia76544 <83532024+cordelia76544@users.noreply.github.com>
2024-06-30 16:01:42 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.howardmcserver.top": dockerHub,
+  "quay.howardmcserver.top": "https://quay.io",
+  "gcr.howardmcserver.top": "https://gcr.io",
+  "k8s-gcr.howardmcserver.top": "https://k8s.gcr.io",
+  "k8s.howardmcserver.top": "https://registry.k8s.io",
+  "ghcr.howardmcserver.top": "https://ghcr.io",
+  "cloudsmith.howardmcserver.top": "https://docker.cloudsmith.io",
+  "ecr.howardmcserver.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.howardmcserver.top": dockerHub,
 };
 
 function routeByHosts(host) {
